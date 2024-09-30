35874853c925e4249632411ad377123386f4c332
thejaswinidm <thejaswini360@gmail.com>
2024-09-30 18:46:07 -0400

    test

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -123,6 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/var/app/current/static/'
 
 STATIC_ROOT =  'static'
